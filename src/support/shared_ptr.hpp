a9aa47955b7404b650ed08634e84b7a9945058fe
jleahred <jleahred@gmail.com>
2015-04-04 00:46:08 +0200

    fixed errors shared_ptr

--- src/support/shared_ptr.hpp
+++ src/support/shared_ptr.hpp
@@ -15,7 +15,7 @@
 namespace  jle
 {
 
-    
+
 template<typename _Tp>
 class weak_ptr;
 
@@ -29,29 +29,29 @@ class weak_ptr;
     throw
 
     operator bool has also been removed
-    
+
     _No external dependencies_
-    
+
     Example:
     \include ./support/shared_ptr.cpp
 */
 
 template<typename _Tp, std::_Lock_policy _Lp = std::__default_lock_policy>
-class shared_ptr 
+class shared_ptr
 {
 private:
     std::shared_ptr<_Tp>   ptr;
-    
+
 public:
 
     /**
     *  @brief  Construct an empty %shared_ptr.
     *  @post   use_count()==0 && get()==0
     */
-    constexpr shared_ptr() noexcept
+    constexpr shared_ptr() /*noexcept*/
     :  ptr() {}
 
-    shared_ptr(const shared_ptr&) noexcept = default;
+    shared_ptr(const shared_ptr&) /*noexcept*/ = default;
 
     /**
     *  @brief  Construct a %shared_ptr that owns the pointer @a __p.
@@ -60,7 +60,7 @@ public:
     *  @throw  std::bad_alloc, in which case @c delete @a __p is called.
     */
     template<typename _Tp1>
-    explicit shared_ptr(_Tp1* __p) 
+    explicit shared_ptr(_Tp1* __p)
     : ptr(__p) {}
 
 
@@ -96,7 +96,7 @@ public:
    *  The last owner will call __d(__p)
    */
     template<typename _Deleter>
-    shared_ptr(nullptr_t __p, _Deleter __d) 
+    shared_ptr(nullptr_t __p, _Deleter __d)
     : ptr(__p, __d) {}
 
   /**
@@ -155,7 +155,7 @@ public:
 //   * @endcode
 //   */
 //    template<typename _Tp1>
-//    shared_ptr(const shared_ptr<_Tp1>& __r, _Tp* __p) noexcept
+//    shared_ptr(const shared_ptr<_Tp1>& __r, _Tp* __p) /*noexcept*/
 //    : PENDING
 
 
@@ -168,7 +168,7 @@ public:
    */
     template<typename _Tp1, typename = typename
        std::enable_if<std::is_convertible<_Tp1*, _Tp*>::value>::type>
-    shared_ptr(const shared_ptr<_Tp1>& __r) noexcept
+    shared_ptr(const shared_ptr<_Tp1>& __r) /*noexcept*/
     : ptr(__r.ptr) {}
 
   /**
@@ -176,7 +176,7 @@ public:
    *  @param  __r  A %shared_ptr rvalue.
    *  @post   *this contains the old value of @a __r, @a __r is empty.
    */
-    shared_ptr(shared_ptr&& __r) noexcept
+    shared_ptr(shared_ptr&& __r) /*noexcept*/
     : ptr(__r.ptr) {}
 
 
@@ -185,7 +185,7 @@ public:
    *  @param  __r  A %shared_ptr rvalue.
    *  @post   *this contains the old value of @a __r, @a __r is empty.
    */
-    explicit shared_ptr(std::shared_ptr<_Tp>&& __ptr) noexcept
+    explicit shared_ptr(std::shared_ptr<_Tp>&& __ptr) /*noexcept*/
     : ptr(__ptr) {}
 
   /**
@@ -195,7 +195,7 @@ public:
    */
     template<typename _Tp1, typename = typename
        std::enable_if<std::is_convertible<_Tp1*, _Tp*>::value>::type>
-    shared_ptr(shared_ptr<_Tp1>&& __r) noexcept
+    shared_ptr(shared_ptr<_Tp1>&& __r) /*noexcept*/
     : ptr(__r.ptr) {}
 
 
@@ -212,7 +212,7 @@ public:
     : ptr(__r.ptr) {}
 
     template<typename _Tp1, typename _Del>
-    shared_ptr(std::unique_ptr<_Tp1, _Del>&& __r) 
+    shared_ptr(std::unique_ptr<_Tp1, _Del>&& __r)
     : ptr(__r.ptr) {}
 
   /**
@@ -220,17 +220,17 @@ public:
    *  @param  __p  A null pointer constant.
    *  @post   use_count() == 0 && get() == nullptr
    */
-    constexpr shared_ptr(nullptr_t __p) noexcept
+    constexpr shared_ptr(nullptr_t __p) /*noexcept*/
     : ptr(__p){}
 
     explicit shared_ptr(const std::shared_ptr<_Tp>&  _ptr)
     :  ptr(_ptr) {}
 
-    shared_ptr& operator=(const shared_ptr&) noexcept = default;
+    shared_ptr& operator=(const shared_ptr&) /*noexcept*/ = default;
 
     template<typename _Tp1>
     shared_ptr&
-    operator=(const shared_ptr<_Tp1>& __r) noexcept
+    operator=(const shared_ptr<_Tp1>& __r) /*noexcept*/
     {
         ptr=__r;
         return *this;
@@ -238,17 +238,17 @@ public:
 
 
     shared_ptr&
-    operator=(shared_ptr&& __r) noexcept
+    operator=(shared_ptr&& __r) /*noexcept*/
     {
-        ptr =__r;
+        ptr =__r.ptr;
         return *this;
     }
 
     template<class _Tp1>
     shared_ptr&
-    operator=(shared_ptr<_Tp1>&& __r) noexcept
+    operator=(shared_ptr<_Tp1>&& __r) /*noexcept*/
     {
-        ptr = __r;
+        ptr = __r.ptr;
         return *this;
     }
 
@@ -256,7 +256,7 @@ public:
     shared_ptr&
     operator=(std::unique_ptr<_Tp1, _Del>&& __r)
     {
-        ptr = __r;
+        ptr = __r.ptr;
         return *this;
     }
 
@@ -268,7 +268,7 @@ public:
 
 
         void
-        reset() noexcept
+        reset() /*noexcept*/
         { ptr.reset(); }
 
         template<typename _Tp1>
@@ -289,14 +289,14 @@ public:
 
         // Allow class instantiation when _Tp is [cv-qual] void.
         typename std::add_lvalue_reference<_Tp>::type
-        operator*() const noexcept
+        operator*() const
         {
             check_ptr();
             return *ptr;
         }
 
         _Tp*
-        operator->() const noexcept
+        operator->() const
         {
             check_ptr();
             return ptr.get();
@@ -308,25 +308,25 @@ public:
         }
 
         _Tp*
-        get() const noexcept
+        get() const
         {
             check_ptr();
-            return ptr; 
+            return ptr.get();
         }
 
 //        explicit operator bool() const // never throws
 //        { return _M_ptr == 0 ? false : true; }
 //
         bool
-        unique() const noexcept
+        unique() const /*noexcept*/
         { return ptr.unique(); }
 
         long
-        use_count() const noexcept
+        use_count() const /*noexcept*/
         { return ptr.use_count(); }
 
         void
-        swap(std::__shared_ptr<_Tp, _Lp>& __other) noexcept
+        swap(std::__shared_ptr<_Tp, _Lp>& __other) /*noexcept*/
         {
             ptr.swap(__other);
         }
@@ -362,7 +362,7 @@ public:
 //
 //    private:
 //      void*
-//      _M_get_deleter(const std::type_info& __ti) const noexcept
+//      _M_get_deleter(const std::type_info& __ti) const /*noexcept*/
 //      { return ptr._M_get_deleter(__ti); }
 //
 //      template<typename _Tp1>
@@ -379,7 +379,7 @@ public:
 //      template<typename _Tp1, _Lock_policy _Lp1> friend class __weak_ptr;
 //
 //      template<typename _Del, typename _Tp1, _Lock_policy _Lp1>
-//	friend _Del* get_deleter(const __shared_ptr<_Tp1, _Lp1>&) noexcept;
+//	friend _Del* get_deleter(const __shared_ptr<_Tp1, _Lp1>&) /*noexcept*/;
 //
 //      _Tp*	   	   _M_ptr;         // Contained pointer.
 //      __shared_count<_Lp>  _M_refcount;    // Reference counter.
@@ -415,39 +415,39 @@ private:
 template<typename _Tp1, typename _Tp2>
 inline bool
 operator==(const shared_ptr<_Tp1>& __a,
-       const shared_ptr<_Tp2>& __b) noexcept
+       const shared_ptr<_Tp2>& __b) /*noexcept*/
 { return __a.get() == __b.get(); }
 
 template<typename _Tp>
 inline bool
-operator==(const shared_ptr<_Tp>& __a, nullptr_t) noexcept
+operator==(const shared_ptr<_Tp>& __a, nullptr_t) /*noexcept*/
 { return !__a; }
 
 template<typename _Tp>
 inline bool
-operator==(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
+operator==(nullptr_t, const shared_ptr<_Tp>& __a) /*noexcept*/
 { return !__a; }
 
 template<typename _Tp1, typename _Tp2>
 inline bool
 operator!=(const shared_ptr<_Tp1>& __a,
-       const shared_ptr<_Tp2>& __b) noexcept
+       const shared_ptr<_Tp2>& __b) /*noexcept*/
 { return __a.get() != __b.get(); }
 
 template<typename _Tp>
 inline bool
-operator!=(const shared_ptr<_Tp>& __a, nullptr_t) noexcept
+operator!=(const shared_ptr<_Tp>& __a, nullptr_t) /*noexcept*/
 { return (bool)__a; }
 
 template<typename _Tp>
 inline bool
-operator!=(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
+operator!=(nullptr_t, const shared_ptr<_Tp>& __a) /*noexcept*/
 { return (bool)__a; }
 
 template<typename _Tp1, typename _Tp2>
 inline bool
 operator<(const shared_ptr<_Tp1>& __a,
-      const shared_ptr<_Tp2>& __b) noexcept
+      const shared_ptr<_Tp2>& __b) /*noexcept*/
 {
   typedef typename std::common_type<_Tp1*, _Tp2*>::type _CT;
   return std::less<_CT>()(__a.get(), __b.get());
@@ -455,60 +455,60 @@ operator<(const shared_ptr<_Tp1>& __a,
 
 template<typename _Tp>
 inline bool
-operator<(const shared_ptr<_Tp>& __a, nullptr_t) noexcept
+operator<(const shared_ptr<_Tp>& __a, nullptr_t) /*noexcept*/
 { return std::less<_Tp*>()(__a.get(), nullptr); }
 
 template<typename _Tp>
 inline bool
-operator<(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
+operator<(nullptr_t, const shared_ptr<_Tp>& __a) /*noexcept*/
 { return std::less<_Tp*>()(nullptr, __a.get()); }
 
 template<typename _Tp1, typename _Tp2>
 inline bool
 operator<=(const shared_ptr<_Tp1>& __a,
-       const shared_ptr<_Tp2>& __b) noexcept
+       const shared_ptr<_Tp2>& __b) /*noexcept*/
 { return !(__b < __a); }
 
 template<typename _Tp>
 inline bool
-operator<=(const shared_ptr<_Tp>& __a, nullptr_t) noexcept
+operator<=(const shared_ptr<_Tp>& __a, nullptr_t) /*noexcept*/
 { return !(nullptr < __a); }
 
 template<typename _Tp>
 inline bool
-operator<=(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
+operator<=(nullptr_t, const shared_ptr<_Tp>& __a) /*noexcept*/
 { return !(__a < nullptr); }
 
 template<typename _Tp1, typename _Tp2>
 inline bool
 operator>(const shared_ptr<_Tp1>& __a,
-      const shared_ptr<_Tp2>& __b) noexcept
+      const shared_ptr<_Tp2>& __b) /*noexcept*/
 { return (__b < __a); }
 
 template<typename _Tp>
 inline bool
-operator>(const shared_ptr<_Tp>& __a, nullptr_t) noexcept
+operator>(const shared_ptr<_Tp>& __a, nullptr_t) /*noexcept*/
 { return std::less<_Tp*>()(nullptr, __a.get()); }
 
 template<typename _Tp>
 inline bool
-operator>(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
+operator>(nullptr_t, const shared_ptr<_Tp>& __a) /*noexcept*/
 { return std::less<_Tp*>()(__a.get(), nullptr); }
 
 template<typename _Tp1, typename _Tp2>
 inline bool
 operator>=(const shared_ptr<_Tp1>& __a,
-       const shared_ptr<_Tp2>& __b) noexcept
+       const shared_ptr<_Tp2>& __b) /*noexcept*/
 { return !(__a < __b); }
 
 template<typename _Tp>
 inline bool
-operator>=(const shared_ptr<_Tp>& __a, nullptr_t) noexcept
+operator>=(const shared_ptr<_Tp>& __a, nullptr_t) /*noexcept*/
 { return !(__a < nullptr); }
 
 template<typename _Tp>
 inline bool
-operator>=(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
+operator>=(nullptr_t, const shared_ptr<_Tp>& __a) /*noexcept*/
 { return !(nullptr < __a); }
 
 //template<typename _Tp>
@@ -518,23 +518,23 @@ operator>=(nullptr_t, const shared_ptr<_Tp>& __a) noexcept
 // 20.7.2.2.8 shared_ptr specialized algorithms.
 template<typename _Tp>
 inline void
-swap(shared_ptr<_Tp>& __a, shared_ptr<_Tp>& __b) noexcept
+swap(shared_ptr<_Tp>& __a, shared_ptr<_Tp>& __b) /*noexcept*/
 { __a.swap(__b); }
 
 // 20.7.2.2.9 shared_ptr casts.
 template<typename _Tp, typename _Tp1>
 inline shared_ptr<_Tp>
-static_pointer_cast(const shared_ptr<_Tp1>& __r) noexcept
+static_pointer_cast(const shared_ptr<_Tp1>& __r) /*noexcept*/
 { return shared_ptr<_Tp>(__r, static_cast<_Tp*>(__r.get())); }
 
 template<typename _Tp, typename _Tp1>
 inline shared_ptr<_Tp>
-const_pointer_cast(const shared_ptr<_Tp1>& __r) noexcept
+const_pointer_cast(const shared_ptr<_Tp1>& __r) /*noexcept*/
 { return shared_ptr<_Tp>(__r, const_cast<_Tp*>(__r.get())); }
 
 template<typename _Tp, typename _Tp1>
 inline shared_ptr<_Tp>
-dynamic_pointer_cast(const shared_ptr<_Tp1>& __r) noexcept
+dynamic_pointer_cast(const shared_ptr<_Tp1>& __r) /*noexcept*/
 {
   if (_Tp* __p = dynamic_cast<_Tp*>(__r.get()))
 return shared_ptr<_Tp>(__r, __p);
@@ -546,10 +546,10 @@ return shared_ptr<_Tp>(__r, __p);
     @brief  A safe std weak_ptr WRAPPER.
 
     It's almost equal to std::weak_ptr but lock will return a jle::shared_ptr
-    
+
 
     _No external dependencies_
-    
+
 
     Example...
 
@@ -562,7 +562,7 @@ return shared_ptr<_Tp>(__r, __p);
 *  With forwarding constructors and assignment operators.
 */
 template<typename _Tp>
-class weak_ptr 
+class weak_ptr
 {
 private:
     std::weak_ptr<_Tp>  ptr;
@@ -571,24 +571,24 @@ private:
         if (expired())
             throw std::runtime_error("shared_ptr pointer not valid");
     }
-    
+
 public:
-    constexpr weak_ptr() noexcept
+    constexpr weak_ptr() /*noexcept*/
     : ptr() { }
 
     template<typename _Tp1, typename = typename
     std::enable_if<std::is_convertible<_Tp1*, _Tp*>::value>::type>
-    weak_ptr(const weak_ptr<_Tp1>& __r) noexcept
+    weak_ptr(const weak_ptr<_Tp1>& __r) /*noexcept*/
     : ptr(__r) { }
 
     template<typename _Tp1, typename = typename
     std::enable_if<std::is_convertible<_Tp1*, _Tp*>::value>::type>
-    weak_ptr(const shared_ptr<_Tp1>& __r) noexcept
+    weak_ptr(const shared_ptr<_Tp1>& __r) /*noexcept*/
     : ptr(__r.ptr) { }
 
     template<typename _Tp1>
     weak_ptr&
-    operator=(const weak_ptr<_Tp1>& __r) noexcept
+    operator=(const weak_ptr<_Tp1>& __r) /*noexcept*/
     {
         ptr=__r;
         return *this;
@@ -596,14 +596,14 @@ public:
 
     template<typename _Tp1>
     weak_ptr&
-    operator=(const jle::shared_ptr<_Tp1>& __r) noexcept
+    operator=(const jle::shared_ptr<_Tp1>& __r) /*noexcept*/
     {
         ptr=__r.ptr;
         return *this;
     }
 
     shared_ptr<_Tp>
-    lock() const noexcept
+    lock() const
     {
         if(expired())
             return shared_ptr<_Tp>();
@@ -612,17 +612,17 @@ public:
             return jle::shared_ptr<_Tp>(ptr.lock());
         }
     }
-    
+
     bool expired(void) const {
         return ptr.expired();
     }
-    
+
 };
 
 // 20.7.2.3.6 weak_ptr specialized algorithms.
 template<typename _Tp>
 inline void
-swap(weak_ptr<_Tp>& __a, weak_ptr<_Tp>& __b) noexcept
+swap(weak_ptr<_Tp>& __a, weak_ptr<_Tp>& __b) /*noexcept*/
 { __a.swap(__b); }
 
 
@@ -649,12 +649,12 @@ template<typename _Tp>
 class enable_shared_from_this
 {
 protected:
-  constexpr enable_shared_from_this() noexcept { }
+  constexpr enable_shared_from_this() /*noexcept*/ { }
 
-  enable_shared_from_this(const enable_shared_from_this&) noexcept { }
+  enable_shared_from_this(const enable_shared_from_this&) /*noexcept*/ { }
 
   enable_shared_from_this&
-  operator=(const enable_shared_from_this&) noexcept
+  operator=(const enable_shared_from_this&) /*noexcept*/
   { return *this; }
 
   ~enable_shared_from_this() { }
@@ -671,14 +671,14 @@ public:
 private:
   template<typename _Tp1>
 void
-_M_weak_assign(_Tp1* __p, const std::__shared_count<>& __n) const noexcept
+_M_weak_assign(_Tp1* __p, const std::__shared_count<>& __n) const /*noexcept*/
 { _M_weak_this._M_assign(__p, __n); }
 
   template<typename _Tp1>
 friend void
 __enable_shared_from_this_helper(const std::__shared_count<>& __pn,
                  const enable_shared_from_this* __pe,
-                 const _Tp1* __px) noexcept
+                 const _Tp1* __px) /*noexcept*/
 {
   if (__pe != 0)
     __pe->_M_weak_assign(const_cast<_Tp1*>(__px), __pn);
