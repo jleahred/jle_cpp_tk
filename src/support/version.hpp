e759ee4af3dd8171b8501148764e1a63951fe6ff
jleahred <jleahred@gmail.com>
2015-04-18 18:37:33 +0200

    added version.hpp

---
+++ src/support/version.hpp
@@ -0,0 +1,13 @@
+#ifndef JLE_VERSION_HPP_INCLUDED
+#define JLE_VERSION_HPP_INCLUDED
+
+namespace jle
+{
+
+    static const char*  JLE_VERSION  =
+        #include "../../VERSION"
+    ;
+
+};
+
+#endif // JLE_VERSION_HPP_INCLUDED
