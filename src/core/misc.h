5c02eb1a55c86e88d7e5871c6b631ee2872ac176
jleahred <jleahred@gmail.com>
2015-10-24 19:56:05 +0200

    modif  added line number in JLE_COUT_TRACE

--- src/core/misc.h
+++ src/core/misc.h
@@ -2,13 +2,14 @@
 #define JLECPPTK_MISC_H
 
 
+#include<iostream>
 
 #define  JLE_SS(__EXPR__) ((dynamic_cast<std::ostringstream &>(std::ostringstream().seekp(0, std::ios_base::cur) << __EXPR__ )).str())
 
 #define  JLE_HERE  JLE_SS(__PRETTY_FUNCTION__ << "(" << __FILE__ << ":" << __LINE__ << ")")
 
 #define  JLE_TRACE_S(__X__)  #__X__ << " = " << (__X__) <<"; "
-#define  JLE_COUT_TRACE(__X__)  std::cout <<  JLE_TRACE_S(__X__)  << std::endl;
+#define  JLE_COUT_TRACE(__X__)  std::cout <<  __LINE__ << "  "  <<  JLE_TRACE_S(__X__)  << std::endl;
 
 
 namespace jle {
